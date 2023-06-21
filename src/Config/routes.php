76cef463737f46df29cfcf774d404b3bae899270
falcon758 <falcon758@hotmail.com>
2023-06-21 18:21:37 +0100

    Fix module name

--- src/Config/routes.php
+++ src/Config/routes.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 use Phalcon\Mvc\Router\Group as RouterGroup;
 
 $routes = new RouterGroup([
-    'module'     => \Phlexus\Modules\Shop\Module::getModuleName(),
-    'namespace'  => \Phlexus\Modules\Shop\Module::getHandlersNamespace() . '\\Controllers',
+    'module'     => \Phlexus\Modules\Blog\Module::getModuleName(),
+    'namespace'  => \Phlexus\Modules\Blog\Module::getHandlersNamespace() . '\\Controllers',
     'controller' => 'blog',
     'action'     => 'index',
 ]);
